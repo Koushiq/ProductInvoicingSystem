b5b0f1286cfe1f9491f1f5ab8578971136671093
koushiq <koushiq1234@gmail.com>
2023-02-12 19:23:36 +0600

    ef core added in data layer

--- ProductInvoicingSystem/Program.cs
+++ ProductInvoicingSystem/Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using ProductInvoicingSystem.Data.DataProvider;
+
 namespace ProductInvoicingSystem.App
 {
     internal static class Program
@@ -6,12 +10,24 @@ namespace ProductInvoicingSystem.App
         ///  The main entry point for the application.
         /// </summary>
         [STAThread]
-        static void Main()
+        public static void Main()
         {
+            var serviceProvider = new ServiceCollection()
+                .AddLogging()
+                .BuildServiceProvider();
+
+            //serviceProvider.AddDbContext<ProductInvoicingSystemDbContext>(ServiceLifetime.Transient);
+                                 //.AddLogging()
+                                 //.AddSingleton<IFooService, FooService>()
+                                 //.AddSingleton<IBarService, BarService>()
+                                 //.BuildServiceProvider();
+
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
             Application.Run(new LoginWindow());
+
+
         }
     }
 }
